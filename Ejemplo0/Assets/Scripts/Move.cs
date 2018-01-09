183261ece952a0909b297a73235faaf85ea39eaf
alesm97 <32259637+alesm97@users.noreply.github.com>
2018-01-09 10:22:30 +0100

    no message

--- Ejemplo0/Assets/Scripts/Move.cs
+++ Ejemplo0/Assets/Scripts/Move.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-    public float speed = 15f;
+    public float forceValue = 9.8f;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); 
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); 
 	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))*forceValue);
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
 }
