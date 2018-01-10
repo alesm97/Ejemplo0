70b48161f03914f6c26046e94c7fa7bfdd580c94
alesm97 <32259637+alesm97@users.noreply.github.com>
2018-01-10 13:46:36 +0100

    no message

---
+++ Ejemplo0/Assets/Scripts/Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        //distancia entre player y camara
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Ejemplo0/Assets/Scripts/Move.cs
+++ Ejemplo0/Assets/Scripts/Move.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Move : MonoBehaviour {
 
     public float forceValue = 9.8f;
+    public float jumpValue = 0.1f;
     Rigidbody rb;
 
 	// Use this for initialization
@@ -14,7 +15,11 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); 
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            rb.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+        }
+        
 	}
 
     void FixedUpdate()
